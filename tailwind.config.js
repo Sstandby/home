9a65954a8ce8cc0ea0b6576ad4b55ab00cb05079
SStandby <standbyy9@gmail.com>
2023-06-28 13:20:47 -0500

    feat: new level of blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "home-blue": { 400: "#1F2C67", 500: "#1D2645", 600: "#0C0E18" },
+        "home-blue": {
+          300: "#19224d",
+          400: "#1F2C67",
+          500: "#1D2645",
+          600: "#0C0E18",
+        },
         "icon-inactive": "#363F54",
         "icon-active": "#8D91A0",
       },
     },
   },
   plugins: [],
-}
+};
